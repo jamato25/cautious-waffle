10169c441f701b966108d3710eab8588e4cc9d16
jamato25 <james@oceanworks.co>
2025-05-01 11:30:09 -0400

    Move filtering from FE to the db, remove uneeded console logs, remove getElementById

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,34 @@
+import { sql } from "drizzle-orm";
+
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const searchString = searchParams.get("search")?.toLowerCase();
 
-  const data = advocateData;
+  let data;
+  if (searchString) {
+    const searchRegex = `%${searchString}%`;
+
+    // I am aware the typecasting isn't the most efficient solution.  
+    // I would change the specialty field from JSONb to a m:m relationship in the db (depending on the other uses for it)
+    // Otherwise I think there might be a better way to query.  I'm just doing this for time constraints
+    data = await db
+      .select()
+      .from(advocates)
+      .where(
+        sql`LOWER(${advocates.firstName}) LIKE ${searchRegex}
+        OR LOWER(${advocates.lastName}) LIKE ${searchRegex}
+        OR LOWER(${advocates.city}) LIKE ${searchRegex}
+        OR LOWER(${advocates.degree}) LIKE ${searchRegex}
+        OR LOWER(${advocates.specialties}::text) LIKE ${searchRegex}
+        OR LOWER(${advocates.yearsOfExperience}::text) LIKE ${searchRegex}`
+      )
+  } else {
+    data = await db.select().from(advocates);
+  }
 
   return Response.json({ data });
+
 }
